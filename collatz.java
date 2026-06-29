869efe2f136ae9853a2016d957a822d8953a48fa
YANG YEEUN <beautifulsilversheep@gmail.com>
2026-06-29 17:20:57 +0900

    콜라츠 추측

---
+++ collatz.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int num) {
+        int repeat = 0;
+        if(num==1){
+            return 0;
+        }
+        long n = num;
+        while(n != 1){
+            if(n % 2 == 0){
+                n/=2;
+            }else{
+                n = n*3+1;
+            }
+            repeat++;
+            if(repeat > 500){
+                return -1;
+            }
+        }
+        return repeat;
+    }
+}
